7fc063d6cc72cde0918f35f2edd1fea85a8ba05c
MCober27 <mcober27@htomail.com>
2021-09-29 05:58:25 +0200

    Ejercicios Estructuras3 hasta el 5

--- EjerciciosEstructuras3/Ejercicio5.java
+++ EjerciciosEstructuras3/Ejercicio5.java
@@ -1,9 +1,15 @@
 package EjerciciosEstructuras3;
 
+	/*
+	 *	5.- Obtener cuantas veces hay que sumar 0,48 (0,48+0,48+0,48+...etc) para sobrepasar el valor 23,4.
+	 * 
+	 */
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
